9b14060ec39f4e528cb305cd95b0f337433a1cea
xzhuo <yunkzo@gmail.com>
2016-08-16 16:16:02 -0500

    finalize LINE merge script

--- LINE_compile_fasta.py
+++ LINE_compile_fasta.py
@@ -1,9 +1,6 @@
 # merge LINE fragments to full length LINEs. Using EMBOSS water to align overlapping part.
-
 import sys
-import os
 import json
-import re
 import tempfile
 from Bio import SeqIO
 from Bio.Emboss.Applications import WaterCommandline
@@ -37,13 +34,31 @@ with tempfile.NamedTemporaryFile('w+') as tempembl:
     RMdict = SeqIO.to_dict(SeqIO.parse(tempembl, "embl"))
 
     def Align2_pos(fh, aname, bname):
+        # Shitty biopython does not have handy stuff like locatableseq in bioperl.
+        # So I have to parse it to find the start and end.
+        astart = None
+        aend = None
+        bstart = None
+        bend = None
         for line in fh:
             if not line[:1] == "#":
                 linesplit = line.split()
                 if len(linesplit) == 4:
                     if linesplit[0] == aname:
-                        start = linesplit[1]
-                        end = linesplit[3]
+                        start = int(linesplit[1])
+                        end = int(linesplit[3])
+                        if astart is None or astart > start:
+                            astart = start
+                        if aend is None or aend < end:
+                            aend = end
+                    if linesplit[0] == bname:
+                        start = int(linesplit[1])
+                        end = int(linesplit[3])
+                        if bstart is None or bstart > start:
+                            bstart = start
+                        if bend is None or bend < end:
+                            bend = end
+        return(astart, aend, bstart, bend)
 
     def water_c(RMfile, aname, bname):
         with tempfile.NamedTemporaryFile('w+') as tempout:
@@ -57,35 +72,54 @@ with tempfile.NamedTemporaryFile('w+') as tempembl:
                                            outfile=tempout.name)
             # print(water_cline)
             stdout, stderr = water_cline()
-
-            # Shitty biopython does not have handy stuff like locatableseq in bioperl.
-            # So I have to parse it to find the start and end.
             astart, aend, bstart, bend = Align2_pos(tempout, aname, bname)
+            return (astart, aend, bstart, bend)
 
     def get_seq(seq_list, RMfile, RMdict):
         if len(seq_list) == 2:
-            # call water
             aname = seq_list[0]
             bname = seq_list[1]
             aseq = str(RMdict[aname].seq)
             bseq = str(RMdict[bname].seq)
+            # call water
             astart, aend, bstart, bend = water_c(RMfile, aname, bname)
+            # if aend == len(aseq) and bstart == 1 and bend <= 300 and aend - astart <= 300:
+                # seq = aseq + bseq[bend:]
+                # return seq
 
-            if aend == RMdict[aname].length and bstart == 1 and bend <= 180 and aend - astart <= 180: 
-                seq = aseq[:astart-1] + bseq
-                return seq
-            else:
-                print("no! terribly wrong!!")
-                return "NA"
+            seq = aseq + bseq[bend:]
+            return seq
+            # else:
+            #     print("no! something wrong with %s and %s!!" % (aname, bname))
+            #     return "NA"
 
+        if len(seq_list) == 3:
+            aname = seq_list[0]
+            bname = seq_list[1]
+            cname = seq_list[2]
+            aseq = str(RMdict[aname].seq)
+            bseq = str(RMdict[bname].seq)
+            cseq = str(RMdict[cname].seq)
+            # call water
+            astart, aend, bstart, bend = water_c(RMfile, aname, bname)
+            cstart, cend, dstart, dend = water_c(RMfile, bname, cname)
+            # if (aend == len(aseq) and bstart == 1 and bend <= 300 and aend - astart <= 300) and (
+            #    cend == len(bseq) and dstart == 1 and dend <= 300 and cend - cstart <= 300):
+                # seq = aseq + bseq[bend:cstart] + cseq
+                # return seq
+            
+            seq = aseq + bseq[bend:cstart] + cseq
+            return seq
+            # else:
+            #     print("no! somthing wrong with %s, %s and %s!!" % (aname, bname, cname))
+            #     return "NA"
 
-
-
+    outfa = open(struc+'.fa', 'w')
     for name in LINEdict:
         if "other" in LINEdict[name] and len(LINEdict[name]) == 1:
             frag = LINEdict[name]['other']['te']
             seq = str(RMdict[frag].seq)
-            print(">%s\n%s" % (frag, seq))
+            outfa.write(">%s\n%s\n" % (frag, seq))
 
         elif "5end" in LINEdict[name] and "orf2" in LINEdict[name] and "3end" in LINEdict[name]:
             frag1 = LINEdict[name]['5end']['te']
@@ -93,12 +127,12 @@ with tempfile.NamedTemporaryFile('w+') as tempembl:
             frag3 = LINEdict[name]['3end']['te']
             seq_list = [frag1, frag2, frag3]
             seq = get_seq(seq_list, tempembl.name, RMdict)
-            print(">%s\n%s" % (name, seq))
-        elif "other" in LINEdict[name] and "3end" in LINEdict[name] and name[:2] == "L2":
+            outfa.write(">%s\n%s\n" % (name, seq))
+        elif "other" in LINEdict[name] and "3end" in LINEdict[name] and (name[:2] == "L2" or name[:2] == "L3"):
             frag1 = LINEdict[name]['other']['te']
             frag2 = LINEdict[name]['3end']['te']
             seq_list = [frag1, frag2]
             seq = get_seq(seq_list, tempembl.name, RMdict)
-            print(">%s\n%s" % (name, seq))
+            outfa.write(">%s\n%s\n" % (name, seq))
         else:
             print("can't get sequence for %s" % name)
