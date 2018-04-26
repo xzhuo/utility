c4fc71adc87f951438990414d0e6204386b5cddf
xzhuo <xzhuo@gmail.com>
2018-04-26 15:56:48 -0500

    in progress

--- crossmap_parser.py
+++ crossmap_parser.py
@@ -45,6 +45,47 @@ class Region:
         new_frag.to_end = max([x.to_end for x in self.frags])
         self.frags = [new_frag]
 
+    def can_merge(frag1, frag2, distance):
+        return (frag1.from_chr == frag2.from_chr and
+                frag1.from_strand == frag2.from_strand and
+                abs(max(frag1.from_start, frag2.from_start) - min(frag1.from_end, frag2.from_end) < distance) and
+                frag1.to_chr == frag2.to_chr and
+                frag1.to_strand == frag2.to_strand and
+                abs(max(frag1.to_start, frag2.to_start) - min(frag1.to_end, frag2.to_end)) < distance)
+
+    def combine_frags(self, size_limit):
+        ''' Combine frags based on chr, strand, and within size_limit.'''
+        frag_dict = {}
+        for frag in self.frags:
+            key = frag.to_chr + frag.to_strand
+            if key in frag_dict:
+                frag_dict[key].append(frag)
+            else:
+                frag_dict[key] = [frag]
+        for key in frag_dict[key]:
+            split_pos = []
+            for i in range(1, len(frag_dict[key])):
+                if not self.can_merge(frag_dict[key][i - 1], frag_dict[key][i], size_limit):
+                    split_pos.append(i)
+            for i in range(len(split_pos)):
+                deleted_region = frag_dict.pop(key)
+                key_index = key + i
+                if i == 0:
+                    frag_dict[key_index] = deleted_region[:split_pos[i]]
+                else:
+                    frag_dict[key_index] = deleted_region[split_pos[i - 1]:split_pos[i]]
+                if i + 1 not in range(len(split_pos)):
+                    key_index = key + i + 1
+                    frag_dict[key_index] = deleted_region[split_pos[i]:]
+
+
+
+
+
+        for key in frag_dict:
+            to_start_list = [x.to_start for x in frag_dict[key]]
+            to_end_list = [x.to_end for x in frag_dict[key]]
+
 
 class Frag:
     def __init__(self):
@@ -80,15 +121,6 @@ class Frag:
         elif assembly == "to":
             length = self.to_end - self.to_start
         return length
-
-
-def can_merge(frag1, frag2, distance):
-    return (frag1.from_chr == frag2.from_chr and
-            frag1.from_strand == frag2.from_strand and
-            abs(max(frag1.from_start, frag2.from_start) - min(frag1.from_end, frag2.from_end) < distance) and
-            frag1.to_chr == frag2.to_chr and
-            frag1.to_strand == frag2.to_strand and
-            abs(max(frag1.to_start, frag2.to_start) - min(frag1.to_end, frag2.to_end)) < distance)
 
 
 def in_order(frag1, frag2, size_limit):
@@ -149,7 +181,7 @@ def main():
                 else:
                     frag = Frag()
                     frag.digest_line(line)
-                    if can_merge(last_region.frags[-1], frag, args.distance):
+                    if Region.can_merge(last_region.frags[-1], frag, args.distance):
                         last_region.frags[-1].merge_frags(frag)
                     else:
                         last_region.frags.append(frag)
@@ -158,10 +190,6 @@ def main():
         summit_frag_list = region.summit_frag_list()
         if len(summit_frag_list) > 0:
             summit_frag = summit_frag_list[0]
-            core_to_chr = summit_frag.to_chr
-            core_to_start = summit_frag.to_start
-            core_to_end = summit_frag.to_end
-            core_to_strand = summit_frag.to_strand
             uni_region = Region()
             for frag in region.frags:
                 if in_order(frag, summit_frag, args.max):
