7520d8f12fa29a3f9d96023ebfd996b242a03e0d
xzhuo <yunkzo@gmail.com>
2022-04-25 15:05:06 -0500

    add merge bam tags script

---
+++ merge_bam_tags.py
@@ -0,0 +1,58 @@
+import os
+import argparse
+import pysam
+
+
+def attach_tags(bam_file, tag_file, out_file):
+    hash = {}
+    tag_pysam = pysam.AlignmentFile(tag_file, threads = 8)
+    for read in tag_pysam.fetch():
+        try:
+            Mm = read.get_tag("Mm")
+            Ml = read.get_tag("Ml")
+            hash[read.query_name] = {'Mm': Mm, 'Ml': Ml}
+        except:
+            MM = read.get_tag("MM")
+            ML = read.get_tag("ML")
+            hash[read.query_name] = {'MM': MM, 'ML': ML}
+    tag_pysam.close()
+
+    bam = pysam.AlignmentFile(bam_file, threads = 8)
+    out = pysam.AlignmentFile(out_file, "wb", template=bam, threads = 8)
+    for read in bam.fetch():
+        query_name = read.query_name
+        if query_name in hash:
+            try:
+                read.set_tag('Mm', hash[query_name]['Mm'], 'Z')
+                read.set_tag('Ml', hash[query_name]['Ml'], 'B')
+            except:
+                read.set_tag('MM', hash[query_name]['MM'], 'Z')
+                read.set_tag('ML', hash[query_name]['ML'], 'B')
+        out.write(read)
+
+    out.close()
+    bam.close()
+
+def main():
+    parser = argparse.ArgumentParser(description='attach Mm and Ml tags from an unmapped bam file to another bam file')
+    parser.add_argument('-b', '--bam', type=str, required=True,
+                        help='input bam file without the desired tags')
+    parser.add_argument('-t', '--tag', type=str, required=True,
+                        help='bam file with tags')
+    parser.add_argument('-o', '--out', type=str, required=True,
+                        help='output bam file with Ml and Mm tags attached')
+
+    args = parser.parse_args()
+    tag_file = os.path.abspath(args.tag)
+    bam_file = os.path.abspath(args.bam)
+    if not os.path.exists(tag_file):
+        raise ValueError("--tag file does not exist!")
+    if not os.path.exists(bam_file):
+        raise ValueError("--bam file does not exist!")
+    
+    outfile = args.out
+    attach_tags(bam_file, tag_file, outfile)
+
+
+if __name__ == '__main__':
+    main()
