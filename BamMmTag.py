d4859a1fe483ec32a2f7c3e3675fb4cd0b54b1ac
xzhuo <yunkzo@gmail.com>
2022-04-29 15:11:14 -0500

    Change Mm tag

---
+++ BamMmTag.py
@@ -0,0 +1,31 @@
+import pysam
+
+
+def modify_tags(bam_file, out_file):
+    bam = pysam.AlignmentFile(bam_file, threads = 8)
+    out = pysam.AlignmentFile(out_file, "wb", template=bam, threads = 8)
+    for read in bam.fetch(until_eof=True):
+        Mm = read.get_tag("Mm")
+        if Mm[3]=="?":
+            Mm = Mm[:3]+Mm[4:]
+        read.set_tag('Mm', Mm)
+        out.write(read)
+
+    out.close()
+    bam.close()
+
+def main():
+    parser = argparse.ArgumentParser(description='Change C+m? to C+m in bam file')
+    parser.add_argument('-b', '--bam', type=str, required=True,
+                        help='input bam file')
+    parser.add_argument('-o', '--out', type=str, required=True,
+                        help='output bam file with Ml and Mm tags corrected')
+
+    args = parser.parse_args()
+    bam_file = args.bam
+    out_file = args.out
+    modify_tags(bam_file, out_file)
+
+
+if __name__ == '__main__':
+    main()
