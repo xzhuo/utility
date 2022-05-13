f7c7c5222cf79cb349388cb4b66b36cc09fbd550
xzhuo <yunkzo@gmail.com>
2022-05-13 18:22:53 -0500

    query pos

--- samToAxt.py
+++ samToAxt.py
@@ -1,3 +1,4 @@
+from turtle import left
 import pysam
 import argparse
 
@@ -11,11 +12,17 @@ def attach_query_seq(bam_file, fasta_file, out_file):
     bam = pysam.AlignmentFile(bam_file)
     out = open(out_file, "w")
     for idx, read in enumerate(bam.fetch()):
+        cigar = read.cigartuples
+        left_clip_length = cigar[0][1] if cigar[0][0] == 5 else 0
+        right_clip_length = cigar[-1][1] if cigar[-1][0] == 5 else 0
+        start = right_clip_length if read.is_reverse else left_clip_length
+        query_start = start
+        query_end = start + read.query_length
         pairs = read.get_aligned_pairs()
         strand = "-" if read.is_reverse else "+"
         query_seq = ''.join(["-" if pos[0] == None else read.query_sequence[pos[0]] for pos in pairs])
         ref_seq = ''.join(["-" if pos[1] == None else fasta.fetch(reference=read.reference_name, start=pos[1], end=pos[1]+1) for pos in pairs])
-        cols = ' '.join(map(str, [idx, read.reference_name, read.reference_start, read.reference_end, read.query_name, read.query_alignment_start, read.query_alignment_end, strand, "60"]))
+        cols = ' '.join(map(str, [idx, read.reference_name, read.reference_start, read.reference_end, read.query_name, query_start, query_end, strand, "60"]))
         output = cols+"\n"+query_seq+"\n"+ref_seq+"\n\n"
         out.write(output)
     out.close()
