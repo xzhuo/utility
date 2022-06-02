5bf7dd89afd9f5428b7af240f913a0ddecb852f6
xzhuo <yunkzo@gmail.com>
2022-06-02 12:47:47 -0500

    add option to attach tags to hard clipped reads

--- merge_bam_tags.py
+++ merge_bam_tags.py
@@ -1,5 +1,6 @@
 import os
 import argparse
+from xmlrpc.client import boolean
 import pysam
 from copy import copy
 
@@ -7,7 +8,7 @@ def revcom(seq):
     tab = str.maketrans("ACGTacgt", "TGCAtgca")
     return seq.translate(tab)[::-1]
 
-def attach_tags(bam_file, tag_file, out_file):
+def attach_tags(bam_file, tag_file, out_file, all_reads=False):
     hash = {}
     if os.path.isfile(tag_file):
         tag_files = [tag_file]
@@ -50,42 +51,45 @@ def attach_tags(bam_file, tag_file, out_file):
                 Ml_array = copy(hash[query_name]['Ml'])
             except:
                 Ml_array = copy(hash[query_name]['ML'])
-            if not read.infer_query_length == read.infer_read_length:
-                # skip reads that are not full length (hard clipped).
-                pass
-                # cigar = read.cigartuples
-                # clip_process = False
-                # clip_length = 0
-                # if (not read.is_reverse) and cigar[0][0] == 5:
-                #     clip_length = cigar[0][1]
-                #     clip_process = True
-                # if read.is_reverse and cigar[-1][0] == 5:
-                #     clip_length = cigar[-1][1]
-                #     clip_process = True
+            if read.infer_query_length == read.infer_read_length:
+                read.set_tag('Mm', Mm_string, 'Z')
+                read.set_tag('Ml', Ml_array)
+            elif all_reads:
+                cigar = read.cigartuples
+                clip_process = False
+                clip_length = 0
+                if (not read.is_reverse) and cigar[0][0] == 5:
+                    clip_length = cigar[0][1]
+                    clip_process = True
+                if read.is_reverse and cigar[-1][0] == 5:
+                    clip_length = cigar[-1][1]
+                    clip_process = True
 
-                # if clip_process:
-                #     clip_seq = hash[query_name]['seq'][:clip_length]
-                #     numC = clip_seq.count('C') + clip_seq.count('c')
+                if clip_process:
+                    clip_seq = hash[query_name]['seq'][:clip_length]
+                    numC = clip_seq.count('C') + clip_seq.count('c')
 
-                #     Mm_string_tail = ""
-                #     if Mm_string[-1:] == ";":
-                #         Mm_string = Mm_string[:-1]
-                #         Mm_string_tail = ";"
+                    Mm_string_tail = ""
+                    if Mm_string[-1:] == ";":
+                        Mm_string = Mm_string[:-1]
+                        Mm_string_tail = ";"
                     
-                #     Mm_list = Mm_string.split(",")
-                #     Mm_type = Mm_list.pop(0)
-                #     Mm_list = list(map(int, Mm_list))
-                #     while numC > 0 and len(Mm_list) > 0:
-                #         if Mm_list[0]>=numC:
-                #             Mm_list[0]-=numC
-                #             numC = 0
-                #         else:
-                #             first = Mm_list.pop(0)
-                #             numC -= first+1
-                #             Ml_array.pop(0)
-                #     Mm_list = list(map(str, Mm_list))
-                #     Mm_list.insert(0, Mm_type)
-                #     Mm_string = ','.join(Mm_list) + Mm_string_tail
+                    Mm_list = Mm_string.split(",")
+                    Mm_type = Mm_list.pop(0)
+                    Mm_list = list(map(int, Mm_list))
+                    while numC > 0 and len(Mm_list) > 0:
+                        if Mm_list[0]>=numC:
+                            Mm_list[0]-=numC
+                            numC = 0
+                        else:
+                            first = Mm_list.pop(0)
+                            numC -= first+1
+                            Ml_array.pop(0)
+                    Mm_list = list(map(str, Mm_list))
+                    Mm_list.insert(0, Mm_type)
+                    Mm_string = ','.join(Mm_list) + Mm_string_tail
+                read.set_tag('Mm', Mm_string, 'Z')
+                read.set_tag('Ml', Ml_array)
 
             read.set_tag('Mm', Mm_string, 'Z')
             read.set_tag('Ml', Ml_array)
@@ -102,7 +106,8 @@ def main():
                         help='bam file with tags, or dir with these bam files')
     parser.add_argument('-o', '--out', type=str, required=True,
                         help='output bam file with Ml and Mm tags attached')
-
+    parser.add_argument('-a', '--all', type=bool, required=False, default=False, action='store_true',
+                        help='Attach Ml and Mm tags to all reads, including hard-clipped reads')
     args = parser.parse_args()
     tag_file = os.path.abspath(args.tag)
     bam_file = os.path.abspath(args.bam)
@@ -112,7 +117,7 @@ def main():
         raise ValueError("--bam file does not exist!")
     
     outfile = args.out
-    attach_tags(bam_file, tag_file, outfile)
+    attach_tags(bam_file, tag_file, outfile, args.all)
 
 
 if __name__ == '__main__':
